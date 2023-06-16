4090cb9db45a32e1f2e0aaff29ccc66262c33f9f
Arthur Doichev <arthurlirity@gmail.com>
2023-06-16 17:41:28 +0200

    1

--- Front-End/L13_JS_JSONPlaceHolder_17.05.2023/script.js
+++ Front-End/L13_JS_JSONPlaceHolder_17.05.2023/script.js
@@ -5,72 +5,70 @@ const userDetailsDiv = document.getElementById("userDetails");
 const searchInput = document.getElementById("searchInput");
 const users = [];
 
-userList.classList.add('userList');
-userList2.classList.add('userList2');
+userList.classList.add("userList");
+userList2.classList.add("userList2");
 
 // users.push(null); // изменять содержимое константы можно
 // users = []; // переопределять константу нельзя
 
 fetch(url)
-    .then((res) => res.json())
-    .then((users) => {
-        users.forEach((user) => {
-            const li = document.createElement("li");
-            li.innerText = user.name;
-            li.addEventListener('click', () => displayUsersDetails(user));
-            userList.append(li);
-        });
-    })
-    .catch((err) => console.log(err));
-
-
+  .then((res) => res.json())
+  .then((users) => {
+    users.forEach((user) => {
+      const li = document.createElement("li");
+      li.innerText = user.name;
+      li.addEventListener("click", () => displayUsersDetails(user));
+      userList.append(li);
+    });
+  })
+  .catch((err) => console.log(err));
 
 fetch(url)
-    .then((res) => res.json())
-    .then((users) => {
-        let filteredUsers = users;
-        displayUsers(filteredUsers);
+  .then((res) => res.json())
+  .then((users) => {
+    let filteredUsers = users;
+    displayUsers(filteredUsers);
 
-        searchInput.addEventListener("input", () => {
-            const searchTerm = searchInput.value.toLowerCase();
-            filteredUsers = users.filter((user) =>
-                user.name.toLowerCase().includes(searchTerm)
-            );
-            displayUsers(filteredUsers);
-        });
-    })
-    .catch((error) => {
-        console.log("Error fetching user", error);
+    searchInput.addEventListener("input", () => {
+      const searchTerm = searchInput.value.toLowerCase();
+      filteredUsers = users.filter((user) =>
+        user.name.toLowerCase().includes(searchTerm)
+      );
+      displayUsers(filteredUsers);
     });
+  })
+  .catch((error) => {
+    console.log("Error fetching user", error);
+  });
 
 function displayUsers(users) {
-    userList2.innerHTML = "";
+  userList2.innerHTML = "";
 
-    users.forEach((user) => {
-        const li = document.createElement("li");
-        li.innerText = user.name;
-        li.addEventListener('click', () => displayUsersDetails(user));
-        userList2.append(li);
-    });
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    li.addEventListener("click", () => displayUsersDetails(user));
+    userList2.append(li);
+  });
 }
 
 function displayUsersDetails(user) {
-    userDetailsDiv.innerHTML = '';
-    
-    const nameHeading = document.createElement('h2');
-    nameHeading.innerHTML = user.name;
+  userDetailsDiv.innerHTML = "";
 
-    const nickName = document.createElement('p');
-    nickName.innerHTML = `<strong>Username: </strong> ${user.username}`;
+  const nameHeading = document.createElement("h2");
+  nameHeading.innerHTML = user.name;
 
-    const email = document.createElement('p');
-    email.innerHTML = `<strong>Email: </strong> ${user.email}`;
+  const nickName = document.createElement("p");
+  nickName.innerHTML = `<strong>Username: </strong> ${user.username}`;
 
-    const phone = document.createElement('p');
-    phone.innerHTML = `<strong>Phone: </strong> ${user.phone}`;
+  const email = document.createElement("p");
+  email.innerHTML = `<strong>Email: </strong> ${user.email}`;
 
-    userDetailsDiv.append(nameHeading, nickName, email, phone);
-    userDetailsDiv.classList.add('userDetailsDiv');
+  const phone = document.createElement("p");
+  phone.innerHTML = `<strong>Phone: </strong> ${user.phone}`;
+
+  userDetailsDiv.append(nameHeading, nickName, email, phone);
+  userDetailsDiv.classList.add("userDetailsDiv");
 }
 
 "Hello world".includes("llo"); // true, ищет в строке совпадения на подстроку
--- Front-End/L13_JS_Promises_17.05.2023/script.js
+++ Front-End/L13_JS_Promises_17.05.2023/script.js
@@ -1,38 +1,40 @@
 function getFiveInSevenSec() {
-    setTimeout(() => {
-        return 5;
-    }, 7000);
+  setTimeout(() => {
+    return 5;
+  }, 7000);
 }
 
 let result = getFiveInSevenSec();
 console.log(result); // undefined - потому что синхронный код не дождался значения
 
 let promise = new Promise(function (resolved, reject) {
-    setTimeout(() => {
-        resolved(5);
-    }, 7000);
+  setTimeout(() => {
+    resolved(5);
+  }, 7000);
 });
 
 console.log(promise);
 
 promise.then((value) => {
-    console.log(value);
-})
+  console.log(value);
+});
 
 let promise2 = new Promise(function (resolved, reject) {
-    setTimeout(() => {
-        reject(new Error('Ошибка, ничего не пришло назад'));
-    }, 7000);
+  setTimeout(() => {
+    reject(new Error("Ошибка, ничего не пришло назад"));
+  }, 7000);
 });
 
 promise2
-.then((data) => {
+  .then((data) => {
     console.log(data);
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-let url = fetch('https://cataas.com/api/cats')
-.then((value) => value.json())
-.then((data) => console.log(data[0]))
+let url = fetch("https://cataas.com/api/cats")
+  .then((value) => value.json())
+  .then((data) => console.log(data[0]));
+
+  
