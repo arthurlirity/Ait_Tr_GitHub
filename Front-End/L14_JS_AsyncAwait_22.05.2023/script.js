076a2e8f515079e574a67e035aba90396395055e
Arthur Doichev <arthurlirity@gmail.com>
2023-06-30 08:33:54 +0100

    1

--- Front-End/L14_JS_AsyncAwait_22.05.2023/script.js
+++ Front-End/L14_JS_AsyncAwait_22.05.2023/script.js
@@ -10,101 +10,102 @@ const loadPostsBtn = document.getElementById("loadPostsBtn");
 const addPostBtn = document.getElementById("addPostBtn");
 
 loadUsersBtn.addEventListener("click", async () => {
-    const users = await fetchUsers();
-    displayUsers(users);
+  const users = await fetchUsers();
+  displayUsers(users);
 });
 
 loadPostsBtn.addEventListener("click", async () => {
-    const posts = await fetchPosts();
-    displayPosts(posts);
+  const posts = await fetchPosts();
+  displayPosts(posts);
 });
 
 addPostBtn.addEventListener("click", handleAddPost); // заменил второй атрибут на простой вызов необходимой функции
 
 async function fetchUsers() {
-    try {
-        const response = await fetch(`${baseUrl}users`);
-        const users = response.json();
-        return users;
-    } catch (error) {
-        console.log("Error fetching users: ", error);
-    }
+  try {
+    const response = await fetch(`${baseUrl}users`);
+    const users = response.json();
+    return users;
+  } catch (error) {
+    console.log("Error fetching users: ", error);
+  }
 }
 
 async function fetchPosts() {
-    try {
-        const response = await fetch(`${baseUrl}posts`);
-        const posts = response.json();
-        return posts;
-    } catch (error) {
-        console.log("Error fetching users: ", error);
-    }
+  try {
+    const response = await fetch(`${baseUrl}posts`);
+    const posts = response.json();
+    return posts;
+  } catch (error) {
+    console.log("Error fetching users: ", error);
+  }
 }
 
 async function createPost(postData) {
-    try {
-        const response = await fetch(`${baseUrl}posts`, {
-            method: "POST",
-            body: JSON.stringify(postData),
-        });
-        const post = await response.json();
-        return post;
-    } catch (error) {
-        console.log("Error fetching users: ", error);
-    }
+  try {
+    const response = await fetch(`${baseUrl}posts`, {
+      method: "POST",
+      body: JSON.stringify(postData),
+    });
+    const post = await response.json();
+    return post;
+  } catch (error) {
+    console.log("Error fetching users: ", error);
+  }
 }
 
 async function handleAddPost() {
-    if (!title || !body) {
-        alert("Please enter something in field title and body");
-        return;
-    }
+  if (!title || !body) {
+    alert("Please enter something in field title and body");
+    return;
+  }
 
-    const postData = {  // id не нужен, он генерируется сервером
-        title: title.value,
-        body: body.value,
-    };
+  const postData = {
+    // id не нужен, он генерируется сервером
+    title: title.value,
+    body: body.value,
+  };
 
-    const post = await createPost(postData);
+  const post = await createPost(postData);
 
-    if (post) {
-        title.value = "";
-        body.value = "";
-        
-        const posts = await fetchPosts();
-        displayPosts(posts);
-    } else {
-        alert('Error');
-    }
+  if (post) {
+    title.value = "";
+    body.value = "";
+
+    const posts = await fetchPosts();
+    displayPosts(posts);
+  } else {
+    alert("Error");
+  }
 }
 
 function displayPosts(posts) {
-    postsList.innerHTML = "";
+  postsList.innerHTML = "";
 
-    posts.forEach((post) => {
-        const li = document.createElement("li");
-        li.innerHTML = `
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
             <h4>${post.title}</h4>
             <p>${post.body}</p>
         `;
-        postsList.append(li);
-    });
+    postsList.append(li);
+  });
 }
 
 function displayUsers(users) {
-    usersList.innerHTML = "";
+  usersList.innerHTML = "";
 
-    users.forEach(({ name, phone }) => {
-        const li = document.createElement("li");
-        const phoneFormat = phone.split(" ")[0];
-        li.textContent = name + "   " + phoneFormat;
-        usersList.append(li);
-    });
+  users.forEach(({ name, phone }) => {
+    const li = document.createElement("li");
+    const phoneFormat = phone.split(" ")[0];
+    li.textContent = name + "   " + phoneFormat;
+    usersList.append(li);
+  });
 
-    // const alt = users.map(user => {
-    //     const phoneFormat = user.phone.split(' ')[0];
-    //     return `<li>${user.name} ${phoneFormat}</li>`;
-    // }).join('');
+  // const alt = users.map(user => {
+  //     const phoneFormat = user.phone.split(' ')[0];
+  //     return `<li>${user.name} ${phoneFormat}</li>`;
+  // }).join('');
 
-    // userList.innerHTML = alt;
+  // userList.innerHTML = alt;
 }
